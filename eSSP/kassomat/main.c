56c5070d0e8f068b4c6087ddc5fa795247bed118
Andreas Monitzer <andy@monitzer.com>
2015-03-23 03:32:09 +0100

    Integrated libevent into the codebase (using the hiredis adapter). Added a timer that will be used for polling the ssp device.

--- eSSP/kassomat/main.c
+++ eSSP/kassomat/main.c
@@ -1,36 +1,98 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <SSPComs.h>
+#include "port_linux.h"
+#include <signal.h>
 #include <hiredis.h>
+#include <async.h>
+#include <adapters/libevent.h>
 
-#include <stdlib.h>
+void connectToValidator(SSP_COMMAND *sspC);
+void esspPoll(int fd, short event, void *arg);
 
-int main(int argc, char **argv) {
-	fprintf(stderr, "eSSP starting up...\n");
+redisAsyncContext *db;
+struct event sspPollEvent;
+struct event_base *eventBase;
 
-	SSP_COMMAND sspC;
+void cleanup(void) {
+	if(db) {
+		redisAsyncFree(db);
+	}
+	close_ssp_port();
+}
 
-	struct timeval timeout = { 1, 500000 }; // 1.5 seconds
-	redisContext *db = redisConnectWithTimeout("127.0.0.1", 6379, timeout);
+void interrupt(int signal) {
+	evtimer_del(&sspPollEvent);
+	redisAsyncDisconnect(db);
+	db = NULL;
+}
+
+void setupDatabase(void) {
+	db = redisAsyncConnect("127.0.0.1", 6379);
 
 	if(db == NULL || db->err) {
 		if(db) {
 			fprintf(stderr, "Connection error: %s\n", db->errstr);
-			redisFree(db);
 		} else {
 			fprintf(stderr, "Connection error: can't allocate redis context\n");
 		}
 		exit(1);
 	}
-	redisEnableKeepAlive(db);
+}
 
-	redisReply *reply;
+void connectCallback(const redisAsyncContext *c, int status) {
+    if (status != REDIS_OK) {
+        fprintf(stderr, "Database error: %s\n", c->errstr);
+        return;
+    }
+    fprintf(stderr, "Connected to database...\n");
 
-	reply = redisCommand(db,"PING");
-	printf("PING: %s\n", reply->str);
-	freeReplyObject(reply);
+		SSP_COMMAND sspC;
+		sspC.Timeout = 1000;
+	  sspC.EncryptionStatus = NO_ENCRYPTION;
+	  sspC.RetryLevel = 3;
+	  sspC.BaudRate = 9600;
+		connectToValidator(&sspC);
+}
 
+void disconnectCallback(const redisAsyncContext *c, int status) {
+    if (status != REDIS_OK) {
+        fprintf(stderr, "Database error: %s\n", c->errstr);
+        return;
+    }
+    fprintf(stderr, "Disconnected from database\n");
+}
 
-	redisFree(db);
+void connectToValidator(SSP_COMMAND *sspC) {
+	fprintf(stderr, "Connecting to validator...\n");
+
+	struct timeval time;
+	time.tv_sec = 0;
+	time.tv_usec = 500000;
+
+	event_set(&sspPollEvent, 0, EV_PERSIST, esspPoll, NULL);
+	event_base_set(eventBase, &sspPollEvent);
+	evtimer_add(&sspPollEvent, &time);
+}
+
+void esspPoll(int fd, short event, void *arg) {
+	fprintf(stderr, "Poll\n");
+}
+
+int main(int argc, char **argv) {
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGINT, interrupt);
+	eventBase = event_base_new();
+
+	fprintf(stderr, "eSSP starting up...\n");
+	atexit(cleanup);
+	setupDatabase();
+
+	redisLibeventAttach(db, eventBase);
+	redisAsyncSetConnectCallback(db, connectCallback);
+	redisAsyncSetDisconnectCallback(db, disconnectCallback);
+
+	event_base_dispatch(eventBase);
 
 	return 0;
 }
